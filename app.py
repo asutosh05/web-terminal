4606a9dbc1e935a2be12880ee92f640d9eb14646
Asutosh Patel <asutosh407@gmail.com>
2018-04-22 15:11:39 +0530

    Basice UI got data from Airtabel

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request, render_template
+from airtable import Airtable
+
+AT= Airtable('app18kAON3MzboLOJ',
+            'Skills',
+            'keyxk901dRG3cMf4a')
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    search_result=AT.get_all()
+    return render_template('index.html',data=search_result)
+
+if __name__ == '__main__':
+   app.run()
