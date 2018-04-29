bf5236ae2f102eed7451842b18f1f44139f4227c
asutosh05 <asutosh407@gmail.com>
2018-04-29 19:40:44 +0530

    Ajax call

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,jsonify
 from airtable import Airtable
 
-AT= Airtable('app18kAON3MzboLOJ',
-            'Skills',
-            'keyxk901dRG3cMf4a')
+AT= Airtable('Table API Key',
+            'Table Name',
+            'Airtable Api key')
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    search_result=AT.get_all()
     return render_template('index.html',)
+
+@app.route('/search')
+def rate():
+    #query = request.args.get('query')
+    search_result=AT.get_all()
+    return jsonify(search_result)
+    
+
 
 if __name__ == '__main__':
    app.run()
